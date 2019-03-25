c2f325171a0351163030618de312e0bfedfcfe42
Amory Aubert <amory.aubert@gmail.com>
2019-03-26 00:10:09 +0100

    pagination

---
+++ pagination/index.php
@@ -0,0 +1,184 @@
+<?php
+// déclaration d'une variable de contenu, équivalent à un résultat venant d'une DB
+$contenu = array(
+    1 => array(
+        "titre"=>"Chou rouge aux grains d'anis vert",
+        "letexte"=>"Un pique-nique aussi facile à préparer qu’agréable à picorer ? Les recettes se concentrent en temps, pas en plaisir, pour rester plus longtemps sur l’herbe que devant les fourneaux ! 
+				Vite faites, bien faites, les surprises du panier font appel aux magiciens de la cuisine express pour régaler sans compliquer. 
+				Zéro prise de tête pour les rois de la nappe à carreaux, misez sans complexes sur des idées faciles et transpirez sous le soleil plutôt que sous le tablier !.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    2 => array(
+        "titre"=>"Tarte aux pommes à la compote",
+        "letexte"=>"Un pique-nique aussi facile à préparer qu’agréable à picorer ? Les recettes se concentrent en temps, pas en plaisir, pour rester plus longtemps sur l’herbe que devant les fourneaux ! 
+					Vite faites, bien faites, les surprises du panier font appel aux magiciens de la cuisine express pour régaler sans compliquer. 
+					Zéro prise de tête pour les rois de la nappe à carreaux, misez sans complexes sur des idées faciles et transpirez sous le soleil plutôt que sous le tablier !",
+        "ladate"=>"2013-11-15 18:20:00"
+    ),
+    3 => array(
+        "titre"=>"Cake  aux tomates sechées",
+        "letexte"=>"Si le pique-nique se veut rapide, c’est pour mieux profiter du grand air et du soleil. Une excellente raison, donc, pour déballer les jokers de la recette ! 
+		          En tête desquels la tomate, rouge, ronde et gorgée de soleil, prête à aligner sur les carreaux de la nappe des bruschettas colorées de tomate au fromage frais.",
+        "ladate"=>"2014-18-02 21:15:00"
+    ),
+    4 => array(
+        "titre"=>"Tartelettes express aux tomates cerises",
+        "letexte"=>"Me mini tartelettes express aux tomates cerise ou des pâtes aux tomates cerise et basilic en bocal… 
+		          à moins de choisir la version séchée en cake doré aux tomates séchées, pignons de pin et jambon, ou terrine d’agneau aux tomates séchées.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    5 => array(
+        "titre"=>"Madeleines au beurre et citrons",
+        "letexte"=>"De quoi entamer comme il se doit les incontournables recettes bouchères et charcutières, sans stress et sans prise de tête ! 
+		          Impossible de se tromper avec un sandwich au jambon et crudités, pas question de zapper le club sandwich au brie et salam.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    6 => array(
+        "titre"=>"Couscous au citron rouge",
+        "letexte"=>"Une fonction est un bloc de code PHP destiné généralement à être réutilisé plusieurs fois. Plutôt que d'écrire plusieurs fois le même morceau de code, on met celui-ci dans une fonction, et c'est cette fonction que l'on appellera dès que l'on en aura besoin.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    7 => array(
+        "titre"=>"Poissons empoisonnés à l'arsenic",
+        "letexte"=>"Une fonction est un bloc de code PHP destiné généralement à être réutilisé plusieurs fois. Plutôt que d'écrire plusieurs fois le même morceau de code, 
+				on met celui-ci dans une fonction, et c'est cette fonction que l'on appellera dès que l'on en aura besoin.
+				De quoi entamer comme il se doit les incontournables recettes bouchères et charcutières, sans stress et sans prise de tête ! 
+				Impossible de se tromper avec un sandwich au jambon et crudités, pas question de zapper le club sandwich au brie et salami",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    8 => array(
+        "titre"=>"Les septs citrons à la mangue bleu mauve",
+        "letexte"=>"Une fonction est un bloc de code PHP destiné généralement à être réutilisé plusieurs fois. Plutôt que d'écrire plusieurs fois le même morceau de code, on met celui-ci dans une fonction, 
+		          et c'est cette fonction que l'on appellera dès que l'on en aura besoin.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    9 => array(
+        "titre"=>"Neuf",
+        "letexte"=>"De quoi entamer comme il se doit les incontournables recettes bouchères et charcutières, sans stress et sans prise de tête ! 
+				Impossible de se tromper avec un sandwich au jambon et crudités, pas question de zapper le club sandwich au brie et salamiUne fonction est un bloc de code PHP 
+				destiné généralement à être réutilisé plusieurs fois. Plutôt que d'écrire plusieurs fois le même morceau de code, on met celui-ci dans une fonction, 
+				et c'est cette fonction que l'on appellera dès que l'on en aura besoin.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    10 => array(
+        "titre"=>"Dix",
+        "letexte"=>"Une fonction est un bloc de code PHP destiné généralement à être réutilisé plusieurs fois. 
+		Plutôt que d'écrire plusieurs fois le même morceau de code, on met celui-ci dans une fonction, et c'est cette fonction que l'on appellera dès que l'on en aura besoin.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    11 => array(
+        "titre"=>"Onze",
+        "letexte"=>"Une fonction est un bloc de code PHP destiné généralement à être réutilisé plusieurs fois. Plutôt que d'écrire plusieurs fois le même morceau de code, on met celui-ci dans une fonction, et c'est cette fonction que l'on appellera dès que l'on en aura besoin.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    12 => array(
+        "titre"=>"Douze coup de midi à la sauce piquante",
+        "letexte"=>"Une fonction est un bloc de code PHP destiné généralement à être réutilisé plusieurs fois. Plutôt que d'écrire plusieurs fois le même morceau de code, on met celui-ci dans une fonction, et c'est cette fonction que l'on appellera dès que l'on en aura besoin.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    13 => array(
+        "titre"=>"Treize patates et une sexy cuisinière",
+        "letexte"=>"Une fonction est un bloc de code PHP destiné généralement à être réutilisé plusieurs fois. Plutôt que d'écrire plusieurs fois le même morceau de code, 
+		           on met celui-ci dans une fonction, et c'est cette fonction que l'on appellera dès que l'on en aura besoin.",
+        "ladate"=>"2013-11-02 21:15:00"
+    ),
+    14 => array(
+        "titre"=>"La sauce ndole à l'eau brulé",
+        "letexte"=>"Une fonction est un bloc de code PHP destiné généralement à être réutilisé plusieurs fois. Plutôt que d'écrire plusieurs fois le même morceau de code, on met celui-ci dans une fonction, et c'est cette fonction que l'on appellera dès que l'on en aura besoin.",
+        "ladate"=>"2013-11-02 21:15:00"
+    )
+);
+$elements=count($contenu);//total
+$elementsParPage=4;//limit
+$nbPage=ceil($elements/$elementsParPage);//totalpage
+$page = ! empty( $_GET['page'] ) ? (int) $_GET['page'] : 1;
+$page = max($page, 1); //get 1 page when $_GET['page'] <= 0
+$page = min($page, $nbPage); //get last page when $_GET['page'] > $totalPages
+$offset = ($page - 1) * $elementsParPage;
+if( $offset < 0 ) $offset = 0;
+
+$contenu2 = array_slice( $contenu, $offset, $elementsParPage );
+$link = 'index.php?page=%d';
+$pagerContainer = '<div>';
+if( $nbPage != 0 ) 
+{
+  if( $page == 1 ) 
+  { 
+    $pagerContainer .= ''; 
+  } 
+  else 
+  { 
+    $pagerContainer .= sprintf( '<a href="' . $link . '"> &#171; page précédente</a>', $page - 1 ); 
+  }
+  $pagerContainer .= ' <span> page <strong>' . $page . '</strong> sur ' . $nbPage . ' </span>'; 
+  if( $page == $nbPage ) 
+  { 
+    $pagerContainer .= ''; 
+  }
+  else 
+  { 
+    $pagerContainer .= sprintf( '<a href="' . $link . '">page suivante &#187; </a>', $page + 1 ); 
+  }           
+}                   
+$pagerContainer .= '</div>';
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>La pagination</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+<style>
+ *{
+    text-align: center;
+}
+body{
+    background: #ddd;
+}
+main{
+    background: #eee;
+    width: 65%;
+    padding: 0 40px;
+    display: inline-block;
+    border: 1px black solid;
+    border-radius: 10px;
+}
+h3{
+    text-decoration: underline;
+}
+a{
+    color: blue;
+}
+.article{
+    margin: 35px 0 45px;
+    border-bottom: 1px solid #999;
+}
+.date{
+    font-style: italic;
+    padding-right: 50px;
+    text-align: right;
+}
+</style>
+<h2>La pagination</h2>
+<main>
+<?php
+//var_dump($contenu2);
+foreach ($contenu2 as $v1) {
+    //foreach ($v1 as $v2) {
+    //    echo "<p>".$v2."</p>";
+    //}
+    echo "<div class='article'>
+          <h3>".$v1['titre']."</h3>
+          <p class='texte'>".$v1['letexte']."</p>
+          <p class='date'>Posté le ".$v1['ladate']."</p>
+          </div>";
+}
+echo "</main>".$pagerContainer;
+?>
+
+</body>
+</html>
